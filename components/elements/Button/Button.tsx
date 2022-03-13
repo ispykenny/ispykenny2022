90dc5486b6f45100b2ce31d3351e1303e18307a2
Kenny Krosky <kenny.krosky@gmail.com>
2022-03-12 19:37:06 -0800

    fixed secondary button

--- components/elements/Button/Button.tsx
+++ components/elements/Button/Button.tsx
@@ -45,9 +45,7 @@ export const Button: NextPage <Props> = ({ type, url, text, sr }) => {
           <ScreenReader label={sr}/>
           <span>{text}</span>
           {type === 'secondary' && (
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
           )}
         </a>
         </Link>
